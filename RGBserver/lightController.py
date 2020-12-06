ce81344742392d02fe521da5fa312e9a77b2431c
Emil Christiansen <emilsen68@gmail.com>
2020-12-06 19:07:23 +0100

    animation too slow
    Sped up the animation when using color whipe to change color

--- RGBserver/lightController.py
+++ RGBserver/lightController.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
                 print("JSON busy...")
                 time.sleep(0.05)
 
-            colorWipe(strip, Color(data["R"], data["G"], data["B"]))
+            colorWipe(strip, Color(data["R"], data["G"], data["B"]), 10)
 
         #     print ('Color wipe animations.')
         #     colorWipe(strip, Color(255, 0, 0))  # Red wipe
