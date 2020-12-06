5642d01caebc76186fd8eaf8186f2798a29e7996
Emil Christiansen <emilsen68@gmail.com>
2020-12-06 18:32:19 +0100

    Stupid error
    if this fixes the error, then I am stupid

--- RGBserver/lightController.py
+++ RGBserver/lightController.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
         print('Use "-c" argument to clear LEDs on exit')
 
     try:
-        solidColor(strip, Color(255, 255, 255))
+        #solidColor(strip, Color(255, 255, 255))
         while True:
             solidColor(strip, Color(255, 255, 255))
 
