7a23086edc956c41edcd61c18608bd9aa2d5c1dd
Emil Christiansen <emilsen68@gmail.com>
2020-12-19 17:03:21 +0100

    This will work!
    Now I have done research (I googled it), and this should be the way to do it.

--- RGBserver/javascript/translator.js
+++ RGBserver/javascript/translator.js
@@ -5,12 +5,12 @@ let translation = getJSON("/json/langs/norwegian.json")
 // console.log(translation)
 // translate(translation)
 
-for (const key of Object.keys(translation)) {
-    if (key.id == false) {
-        console.log(`Translating to: ${key.language}`)
+for (let key of Object.keys(translation)) {
+    if (key == header) {
+        console.log(`Translating to: ${translation[key].language}`)
     }
     else {
         console.log(key)
-        document.getElementById(key.id).innerHTML = key.translation
+        document.getElementById(key).innerHTML = translation[key]
     }
 }
