033e8299d696eeb3f79b3094dd82ef4e48227974
Emil Christiansen <emilsen68@gmail.com>
2020-12-23 19:24:32 +0100

    Removing autorun suppport
    Using autorun when the script is still in beta souds like a bad idea now that I think about it. For some reason the script can't find what it is looking for when I try to start it otherwise, so I reverted back to how it was before I made it autorun compatible.

--- RGBserver/app.js
+++ RGBserver/app.js
@@ -15,18 +15,15 @@ let connections = []
 
 app.use(cors()) // Making sure the browser can request more data after it is loaded on the client computer.
 
-// Change this path to where the folder containing this file is:
-const folderPath = "home/pi/projects/RGB-controller/RGBserver"
-
 // JSON file loaded in before the server is started:
-let rawdata = fs.readFileSync(path.join(__dirname, "/json/data.json"))
+let rawdata = fs.readFileSync("/json/data.json")
 let data = JSON.parse(rawdata)
 console.log(`Data loaded: ${data}`)
 
 // All server folders are set up:
-app.use("/css", express.static(path.join(__dirname, "/css")))
-app.use("/javascript", express.static(path.join(__dirname, "/javascript")))
-app.use("/json", express.static(path.join(__dirname, "/json")))
+app.use("/css", express.static("/css"))
+app.use("/javascript", express.static("/javascript"))
+app.use("/json", express.static("/json"))
 
 // Graphical control interface:
 app.get("/", (req, res) => {
@@ -72,7 +69,7 @@ app.get("/", (req, res) => {
         if (save) {
             let stringified = JSON.stringify(data, null, 2)
         
-            fs.writeFile(`${folderPath}/json/data.json`, stringified, (err) => {
+            fs.writeFile("/json/data.json", stringified, (err) => {
                 if (err) throw err
                 console.log("Data written to file")
             })
@@ -103,7 +100,7 @@ app.get("/", (req, res) => {
         if (save) {
             let stringified = JSON.stringify(data, null, 2)
         
-            fs.writeFile(`${folderPath}/json/data.json`, stringified, (err) => {
+            fs.writeFile("/json/data.json", stringified, (err) => {
                 if (err) throw err
                 console.log("Data written to file")
             })
@@ -132,7 +129,7 @@ app.get("/lightstate", (req, res) => {
     if (save) {
         let stringified = JSON.stringify(data, null, 2)
     
-        fs.writeFile(`${folderPath}/json/data.json`, stringified, (err) => {
+        fs.writeFile("/json/data.json", stringified, (err) => {
             if (err) throw err
             console.log("Data written to file")
         })
@@ -193,7 +190,7 @@ app.get("/rgb", (req, res) => {
     if (save) {
         let stringified = JSON.stringify(data, null, 2)
     
-        fs.writeFile(`${folderPath}/json/data.json`, stringified, (err) => {
+        fs.writeFile("/json/data.json", stringified, (err) => {
             if (err) throw err
             console.log("Data written to file")
         })
@@ -215,7 +212,7 @@ app.get("/modes/set", (req, res) => {
     if (save) {
         let stringified = JSON.stringify(data, null, 2)
     
-        fs.writeFile(`${folderPath}/json/data.json`, stringified, (err) => {
+        fs.writeFile("/json/data.json", stringified, (err) => {
             if (err) throw err
             console.log("Data written to file")
         })
