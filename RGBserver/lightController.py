b51926dd65c8e71869c2b682e91d01512333702a
Emil Christiansen <emilsen68@gmail.com>
2020-12-23 20:49:27 +0100

    last part of elitus jump animation
    This should be the final part of the elitus jump animation. Now it just has to be integrated on my computer.

--- RGBserver/lightController.py
+++ RGBserver/lightController.py
@@ -327,6 +327,30 @@ def elitus(strip, data):
                 elif br == 100:
                     plus = False
 
+            startTime = datetime.datetime.now().timestamp()
+            while datetime.datetime.now().timestamp() - startTime < 20:
+               color1 = random.randint(0, 255) # Random color 1
+                color2 = random.randint(0, 255) # Random color 2
+                fullColor = random.randint(0, 2) # Choose one of the three RGB channels to be full brightness
+
+                # The RGB channel chosen to be full brightness is set to 255, the others get assigned color1 and color2, wich are randomly generated
+                if fullColor == 0:
+                    r = 255
+                    g = color1
+                    b = color2
+                elif fullColor == 1:
+                    r = color1
+                    g = 255
+                    b = color2
+                elif fullColor == 2:
+                    r = color1
+                    g = color2
+                    b = 255
+                
+                for i in range(strip.numPixels()):
+                    strip.setPixelColor(i, Color(r, g, b))
+                strip.show()
+
 
 
 # Main program logic follows:
