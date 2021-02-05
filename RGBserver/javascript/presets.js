66828487c97ba5251c35a645e34a9a211e850e37
Emil Christiansen <emilsen68@gmail.com>
2021-02-05 14:07:30 +0100

    Addeventlistener twice?
    Don't know how that happened, but i fixed it

--- RGBserver/javascript/presets.js
+++ RGBserver/javascript/presets.js
@@ -1,7 +1,7 @@
 const savePresetButton = document.getElementById("savePreset")
 const presetContainer = document.getElementById("presets")
 
-savePresetButton.addEventListeneraddEventListener("click", (event) => {
+savePresetButton.addEventListener("click", (event) => {
     console.log(R.value)
     console.log(G.value)
     console.log(B.value)
