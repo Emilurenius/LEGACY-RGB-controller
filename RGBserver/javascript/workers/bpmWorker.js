5916f1a3ca6094627f5f0571355db9f591f995bb
Emil Christiansen <emilsen68@gmail.com>
2021-03-04 18:48:11 +0100

    Even more extra wait
    The timestamp seems to be around 10 seconds too early no matter what. I'll just add 10 secs and blame spotify

--- RGBserver/javascript/workers/bpmWorker.js
+++ RGBserver/javascript/workers/bpmWorker.js
@@ -1,6 +1,6 @@
 this.onmessage = (e) => {
     let previousCheck = Date.now()
-    while (Date.now() < e.data.nextSongAt + 5000) {
+    while (Date.now() < e.data.nextSongAt + 10000) {
         if (previousCheck > Date.now() + 5000) {
             previousCheck = Date.now()
             this.postMessage({response: "checkSong"})
