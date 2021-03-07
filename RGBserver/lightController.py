490ba37757e0e48e95cb29383ff9f25952cd163e
Emil Christiansen <emilsen68@gmail.com>
2021-03-07 17:45:07 +0100

    Let's try again
    Something is very wrong here

--- RGBserver/lightController.py
+++ RGBserver/lightController.py
@@ -594,7 +594,7 @@ def screenSync(strip):
             jitterCounter = 0 # Reset jitter if color difference is high enough
 
         # Run this if the threshold was met, or jitter is within acceptable limits:
-        if thresholdMet or thresholdNotHitCounter > maxThresholdNotHit and jitterCounter < maxJitter:
+        if jitterCounter < maxJitter:
             if currentColor[0] < newColor[0]: # Change red channel
                 currentColor[0] += changePerTick
                 if prevRedChange == "dwn": # Jitter detection
