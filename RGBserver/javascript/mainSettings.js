ae0b5f73c447ffa2f400c1c04ee7c75f37ed7e70
Emil Christiansen <emilsen68@gmail.com>
2021-03-18 22:08:42 +0100

    Learning how select tag works
    Just finding out how I check the value

--- RGBserver/javascript/mainSettings.js
+++ RGBserver/javascript/mainSettings.js
@@ -1,6 +1,6 @@
 const colorChangeMode = document.getElementById("colorChangeModeSelect")
 
 colorChangeMode.addEventListener("change", (event) => {
-    console.log(`Current value: ${this.value}`)
+    console.log(`Current value: ${colorChangeMode.selectedIndex}`)
     console.log(event)
 })
