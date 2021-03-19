6aa82ca08a2b67e386df9d5ca77d5d2c55b2d911
Emil Christiansen <emilsen68@gmail.com>
2021-03-19 13:50:51 +0100

    New dropdown style, and stylesheet cleaning
    I added a new style that I ripped off an article for my dropdown list. I will be experimenting with this style and figuring out exactly how I want it. Also removed the .Content at the start of many styles inside the main stylesheet to make those styles usable outside divs with the Content class

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-import time
-print(time.time())
+def func0():
+    # Code for function
+
+def func1():
+    # Code for function
+
+def func2():
+    # Code for function
+
+# Current implementation:
+selected = input("Select function: ")
+
+if selected == "func0":
+    func0()
+elif selected == "func1":
+    func1()
+elif selected == "func2":
+    func2()
+
+# Wanted implementation:
+selected = input("Select function: ")
+runFunction(selected) # This is not valid code. This is the code I can't figure out
