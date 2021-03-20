59c2a55bc01fc3bfc6b9881d9bdef606fb582495
Emil Christiansen <emilsen68@gmail.com>
2021-03-20 20:54:26 +0100

    Brightness for website background
    Added the brightness calculation to the website as well, so that it represents the RGB light's status even better

--- RGBserver/javascript/backgroundColor.js
+++ RGBserver/javascript/backgroundColor.js
@@ -4,7 +4,11 @@ function updateBackgroundColor() {
 
 function updateBackgroundLightState(lightState) {
     if (lightState == true) {
-        document.body.style.backgroundColor = `rgb(${R.value}, ${G.value}, ${B.value})`
+        R = R.value * (brightness.value / 1000)
+        G = G.value * (brightness.value / 1000)
+        B = B.value * (brightness.value / 1000)
+
+        document.body.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
     }
     else {
         document.body.style.backgroundColor = `rgb(0, 0, 0)`
