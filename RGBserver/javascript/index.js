42a6476f9a01b7ece612771ba2216e4dc810a2e6
Emil Christiansen <emilsen68@gmail.com>
2021-04-05 07:56:45 +0200

    Just trying out onchange
    I opted out of using onchange in the beginning because I was reloading the page every time information was sent to the server. I no longer do that, so I will retry this onchange function

--- RGBserver/javascript/index.js
+++ RGBserver/javascript/index.js
@@ -33,22 +33,22 @@ sliderButtonMain.addEventListener("click", (event) => {
     updateBackgroundLightState(getJSON(`${url}/checklightstate`))
 })
 
-brightness.onmouseup = () => {
+brightness.onchange = () => {
     getJSON(`${url}/rgb?br=${brightness.value}`)
     updateBackgroundLightState(getJSON(`${url}/checklightstate`))
 }
 
-R.onmouseup = () => {
+R.onchange = () => {
     getJSON(`${url}/rgb?r=${R.value}`)
     updateBackgroundLightState(getJSON(`${url}/checklightstate`))
 }
 
-G.onmouseup = () => {
+G.onchange = () => {
     getJSON(`${url}/rgb?g=${G.value}`)
     updateBackgroundLightState(getJSON(`${url}/checklightstate`))
 }
 
-B.onmouseup = () => {
+B.onchange = () => {
     getJSON(`${url}/rgb?b=${B.value}`)
     updateBackgroundLightState(getJSON(`${url}/checklightstate`))
 }
