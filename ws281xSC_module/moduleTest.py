8f70e61cdbd33662e19f823f4996cc2d368e1a21
Emil Christiansen <emilsen68@gmail.com>
2021-04-16 15:12:49 +0200

    Server control library
    Starting work on a library for communication with the RGB server from a script running on the same localhost

---
+++ ws281xSC_module/moduleTest.py
@@ -0,0 +1,3 @@
+import ws281xSC
+
+LEDs = ws281xSC.LEDs(149, "http://192.168.1.124:3000/")
---
+++ ws281xSC_module/ws281xSC.py
@@ -0,0 +1,39 @@
+# Library for controlling lights connected to the server in this repository
+import requests
+
+class LEDs:
+    def __init__(self, LEDcount, address):
+        self.LEDcount = LEDcount
+        self.address = address
+        self.LEDdata = {}
+        i = 0
+        while i < self.LEDcount:
+            self.LEDdata[i] = {
+                "br": 0,
+                "r": 0,
+                "g": 0,
+                "b": 0,
+            }
+            i += 1
+    
+    def checkAddress(self): # For debugging
+        return self.address
+
+    def printLEDdata(self): # For debugging
+        for key, value in self.LEDdata.items():
+            print(key, value)
+
+    def percentofLEDs(self, percentage):
+        return int(self.LEDcount * percentage / 100)
+
+    def setPixelData(self, i, br=None, r=None, g=None, b=None):
+        if br:
+            self.LEDdata[i]["br"] = br
+        if r:
+            self.LEDdata[i]["r"] = r
+        if g:
+            self.LEDdata[i]["g"] = g
+        if b:
+            self.LEDdata[i]["b"] = b
+
+    
