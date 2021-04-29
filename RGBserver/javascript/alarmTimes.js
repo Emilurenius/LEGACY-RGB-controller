c1f39197b765128fcbf410f4495bbc04985aef69
Emil Christiansen <emilsen68@gmail.com>
2021-04-29 16:25:09 +0200

    Populate alarmTime dropdowns
    Added some javascript that fills in the missing javascript for the dropdowns for selecting an alarm time

---
+++ RGBserver/javascript/alarmTimes.js
@@ -0,0 +1,26 @@
+function alarmTimes() {
+    const alarmTimeDay = document.getElementById("alarmTimeDay")
+    const alarmTimeHour = document.getElementById("alarmTimeHour")
+    const alarmTimeMinute = document.getElementById("alarmTimeMinute")
+
+    document.createElement("option")
+    let i = 0
+    while (i < 23) {
+        let option = document.createElement("option")
+        option.value = `${i}`
+        option.innerHTML = `${i}`
+        alarmTimeHour.appendChild(option)
+        i++
+    }
+
+    let i = 0
+    while (i < 60) {
+        let option = document.createElement("option")
+        option.value = `${i}`
+        option.innerHTML = `${i}`
+        alarmTimeMinute.appendChild(option)
+        i++
+    }
+}
+
+alarmTimes()
