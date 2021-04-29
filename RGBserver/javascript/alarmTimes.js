4c9bfbc643688c3e82edc5264a4225298de23481
Emil Christiansen <emilsen68@gmail.com>
2021-04-29 22:03:48 +0200

    gave id to wrong element
    Things happen. Refer to previous commit for more info

--- RGBserver/javascript/alarmTimes.js
+++ RGBserver/javascript/alarmTimes.js
@@ -28,7 +28,7 @@ function alarmTimes() {
             const deleteIcon = document.createElement("i")
             deleteIcon.classList.add("fa")
             deleteIcon.classList.add("fa-trash")
-            deleteButton.id = alarms[i]
+            deleteIcon.id = alarms[i]
 
             deleteButton.appendChild(deleteIcon)
             alarmText.appendChild(deleteButton)
