ae4fc24c3ea0e08a56076977eb9976b7488c1081
Emil Christiansen <emilsen68@gmail.com>
2021-05-17 21:39:33 +0200

    Removed old code, and exposed settings
    I removed the old code that found the most frequent color, as that is no longer in use. Also exposed the "step" variable that changes the speed and presicion of the script. This allows for custom speed and presicion for computers so the script can run smoothly on any computer.

--- MonitorColorScanner.py
+++ MonitorColorScanner.py
@@ -1,6 +1,6 @@
 # This code is made to run on the user's computer. Not on the server unit
 
-import numpy as np, cv2, requests
+import numpy as np, requests
 from PIL import ImageGrab
 
 def getAverageRGB(colors):
@@ -14,31 +14,8 @@ def getAverageRGB(colors):
 
     return [average[0]/count, average[1]/count, average[2]/count]
 
-def mostFrequent(colorList):
-    highestFreq = 0
-    if colorList: # Check if the colorList has any RGB values in it
-        color = colorList[0] # initiate color variable with the first RGB value
-        color2 = None
-        color3 = None
-
-        for i in colorList: # Iterate through the list
-            currFreq = colorList.count(i) # Count how many times the current color occurs in the list
-            if currFreq > highestFreq: # Check if it appears more often than all other colors checked
-                highestFreq = currFreq # Set new value for highest frequency
-                # Save the the new highest frequency color, and move the two previous ones down to the other variables:
-                color3 = color2
-                color2 = color
-                color = i
-
-        if color2 == None: # Make sure all three variables have a color in them
-            color2 = color
-        if color3 == None:
-            color3 = color2
-
-        return getAverageRGB([color, color2, color3]) # Return the average of the 3 most frequent colors
-    return [0, 0, 0] # Return black if the list of colors is empty
-
-step = 25 # How many pixels to skip in both x and y direction when sampling colors
+print("Please give a speed/presicion modifier value:")
+step = int(input("Higher value = higher speed, Lower value = more presicion: ")) # How many pixels to skip in both x and y direction when sampling colors
 xPixels = 1344 # Number of pixels in the x direction of the checked area
 yPixels = 756 # Number of pixels in the y direction of the checked area
 serverAddress = "http://192.168.1.124:3000" # Address for the main server that controls all info about the LED strip
@@ -58,9 +35,10 @@ while True: # Main script loop
         for x in range(0, xPixels, step): # Iterate through all pixels in the x direction, skipping the amount specified in step for every iteration
             px = imArr[y][x] # Grab pixel at current x and y coordinates
 
-            if px[0] != 0 and px[1] != 0 and px[2] != 0:
-                pixelArray.append([px[0], px[1], px[2]]) # Add the color if it is not complete black
+            # if px[0] != 0 and px[1] != 0 and px[2] != 0:
+            #     pixelArray.append([px[0], px[1], px[2]]) # Add the color if it is not complete black
+            pixelArray.append([px[0], px[1], px[2]])
 
-    mostFrequentColor = mostFrequent(pixelArray) # Get most frequent color in list
+    mostFrequentColor = getAverageRGB(pixelArray) # Get most frequent color in list
     print(mostFrequentColor)
     requests.get(f"{serverAddress}/rgb?br=1000&r={mostFrequentColor[0]}&g={mostFrequentColor[1]}&b={mostFrequentColor[2]}") # Send the most frequent color to the main server
