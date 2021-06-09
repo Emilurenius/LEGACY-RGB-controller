a4242a8c8d51a97873566f5ee1172ce56ede9050
Emil Christiansen <emilsen68@gmail.com>
2021-06-09 09:00:30 +0200

    Python module
    Added the code needed to interface with the new directRGB mode in the module. It's tested, and almost works flawlessly. Gotta iron out the part of the code that makes sure it only sends the values that have changed, and not all the RGB values every tick

--- externalControl/ws281xSC_module/moduleTest.py
+++ externalControl/ws281xSC_module/moduleTest.py
@@ -1,5 +1,20 @@
-import ws281xSC
+import ws281xSC, time
+from datetime import datetime
 
 LEDs = ws281xSC.LEDs(149, "http://192.168.1.124:3000")
 
-print(LEDs.getJSON("data.json"))
+i = 0
+prevLED = None
+while True:
+    start = datetime.now()
+    LEDs.setPixelData(i, 255, 0, 0)
+    if prevLED != None:
+        LEDs.setPixelData(prevLED, 0, 0, 0)
+    prevLED = i
+    i +=1
+
+    if i > LEDs.LEDcount - 1:
+        i = 0
+
+    LEDs.show()
+    print(datetime.now() - start)
--- externalControl/ws281xSC_module/ws281xSC.py
+++ externalControl/ws281xSC_module/ws281xSC.py
@@ -1,5 +1,5 @@
 # Library for controlling lights connected to the server in this repository
-import requests
+import requests, copy
 
 class LEDs:
     def __init__(self, LEDcount, address):
@@ -8,13 +8,9 @@ class LEDs:
         self.LEDdata = {}
         i = 0
         while i < self.LEDcount:
-            self.LEDdata[i] = {
-                "br": 0,
-                "r": 0,
-                "g": 0,
-                "b": 0,
-            }
+            self.LEDdata[i] = [0, 0, 0]
             i += 1
+        self.prevLEDdata = copy.deepcopy(self.LEDdata)
     
     def checkAddress(self): # For debugging
         return self.address
@@ -46,12 +42,20 @@ class LEDs:
             else:
                 requests.get(f"{self.address}/lightstate?toggle=change")
 
-    def setPixelData(self, i, br=None, r=None, g=None, b=None):
-        if br:
-            self.LEDdata[i]["br"] = br
-        if r:
-            self.LEDdata[i]["r"] = r
-        if g:
-            self.LEDdata[i]["g"] = g
-        if b:
-            self.LEDdata[i]["b"] = b
+    def setPixelData(self, i, r=None, g=None, b=None):
+        if r != None:
+            self.LEDdata[i][0] = r
+        if g != None:
+            self.LEDdata[i][1] = g
+        if b != None:
+            self.LEDdata[i][2] = b
+
+    def show(self, onlyChanged=False):
+        # changedData = {}
+        # for k, v in self.LEDdata.items():
+        #     if self.LEDdata[k] != self.prevLEDdata[k]:
+        #         changedData[k] = self.LEDdata[k]
+
+        # requests.post(f"{self.address}/directRGB", changedData)
+
+        requests.post(f"{self.address}/directRGB", self.LEDdata)
