e5dd60697823ecaafe0f185fe5541cfddf86e28f
Emil Christiansen <emilsen68@gmail.com>
2021-07-27 14:14:12 +0200

    Forgot to upload this
    This should have been uploaded a while ago

--- externalControl/ws281xSC_module/moduleTest.py
+++ externalControl/ws281xSC_module/moduleTest.py
@@ -7,25 +7,6 @@ strip.setMode("directRGB")
 r = 0
 g = 0
 b = 0
-while True:
 
-    r+=15
-
-    if r > 255:
-        r = 0
-        g+=15
-
-    if g > 255:
-        g = 0
-        b+=15
-
-    if b > 255:
-        time.sleep(1)
-        break
-
-    i = 0
-    while i < strip.LEDcount:
-        strip.setPixelData(i, r, g, b)
-        i+=1
-    print(r, g, b)
-    strip.show()
+strip.setMode("directRGB")
+strip.show()
