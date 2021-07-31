40ab21f74c1abdb7d59fd59845976419371b6115
Emil Christiansen <emilsen68@gmail.com>
2021-07-31 23:41:36 +0200

    Updated timeout
    The test to check if the address is on the network has been made as fast as it can reliably be

--- findAddressTest.py
+++ findAddressTest.py
@@ -2,11 +2,11 @@ import requests, eventlet
 eventlet.monkey_patch()
 
 serverAddress0 = 1
-serverAddress1 = 120
+serverAddress1 = 0
 
 while True:
     try:
-        with eventlet.Timeout(1):
+        with eventlet.Timeout(0.1):
             res = requests.get(f"http://192.168.{serverAddress0}.{serverAddress1}:3000/ping").json()
             if res["serverName"] == "lightController":
                 print("Address found")
