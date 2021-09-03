b692ab9194307ab8d430f499cb345fb72a78f810
Emil Christiansen <emilsen68@gmail.com>
2021-09-03 12:20:57 +0200

    wrong json name

--- RGBserver/testController.py
+++ RGBserver/testController.py
@@ -681,7 +681,7 @@ def bpm(strip): # [0,150,255], [170, 0, 255]
 
 def songAnimation(strip):
     print("animation started")
-    print(getJSON("songAnimation"))
+    print(getJSON("animatedSongs"))
     # for k, v in animatedSongs:
     #     print(f"uri: {k}.. Path: {v['animationFilepath']}.. fileName: {v['animationFilename']}")
 
